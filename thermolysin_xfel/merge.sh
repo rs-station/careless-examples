9d624ca10fe6e3fd80aa026a94d7b233ad71ef73
Kevin Dalton <kmdalton@pop-os.localdomain>
2022-02-25 13:17:35 -0500

    update iterations in xfel example

--- thermolysin_xfel/merge.sh
+++ thermolysin_xfel/merge.sh
@@ -5,7 +5,7 @@ careless mono \
   --mlp-layers=10 \
   --image-layers=2 \
   --dmin=1.8 \
-  --iterations=30 \
+  --iterations=30_000 \
   "dHKL,xobs,yobs" \
   unmerged.mtz \
   merge/thermolysin
@@ -18,7 +18,7 @@ careless mono \
   --mlp-layers=10 \
   --image-layers=2 \
   --dmin=1.8 \
-  --iterations=30 \
+  --iterations=30_000 \
   "dHKL,xobs,yobs,ewald_offset" \
   unmerged.mtz \
   merge_eo/thermolysin
